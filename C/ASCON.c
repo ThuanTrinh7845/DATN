72897b3621bc421bd6ed8d5c1ef4fc609657ca31
Trinh Vinh Thuan <thuantrinh7845@gmail.com>
2025-09-05 19:22:58 +0700

    encrypt + decrypt C

--- C/ASCON.c
+++ C/ASCON.c
@@ -95,45 +95,83 @@ void initialization(bit64 state[5], bit64 key[2])
     state[4] ^= key[1];
 }
 
-void Plaintext(bit64 plaintext[], int length, bit64 ciphertext[], bit64 state[5])
+void Associated(bit64 state[5], int length, bit64 associated_text[])
 {
-    ciphertext[0] = plaintext[0] ^ state[0];
-    for (int i = 1; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
+        state[0] ^= associated_text[i];
         p(state, 6);
-        ciphertext[i] = plaintext[i] ^ state[0];
-        state[0] = ciphertext[i];
     }
+    state[4] ^= 0x0000000000000001;
 }
+
 
 void Finalization(bit64 state[5], bit64 key[2])
 {
-    state[0] ^= key[1];
-    state[1] ^= key[2];
+    state[1] ^= key[0];
+    state[2] ^= key[1];
     p(state, 12);
+    state[3] ^= key[0];
+    state[4] ^= key[1];
 }
 
-void ascon(bit64 nonce[2], bit64 key[2], bit64 IV, bit64 plaintext[], bit64 ciphertext[], bit64 state[5])
+void Plaintext(bit64 state[5], int length, bit64 plaintext[], bit64 ciphertext[]) {
+   ciphertext[0] = plaintext[0] ^ state[0];
+   for (int i = 1; i < length; i++){
+      p(state, 6);
+      ciphertext[i] = plaintext[i] ^ state[0];
+      state[0] = ciphertext[i];
+   }
+}
+
+void Ciphertext(bit64 state[5], int length, bit64 plaintext[], bit64 ciphertext[]){
+   plaintext[0] = ciphertext[0] ^ state[0];
+   for (int i = 1; i < length; i++){
+      p(state, 6);
+      plaintext[i] = ciphertext[i] ^ state[0];
+      state[0] = ciphertext[i];
+   }
+}
+
+void encrypt(bit64 IV, bit64 state[], bit64 key[], bit64 nonce[], bit64 plaintext[], bit64 ciphertext[], bit64 associated_data_text[])
 {
     state[0] = IV;
     state[1] = key[0];
     state[2] = key[1];
     state[3] = nonce[0];
     state[4] = nonce[1];
-    initialization(state, key);
+    initialization(state,key);
+    Associated(state, 3, associated_data_text);
     print_state(state);
-    Plaintext(plaintext, 2, ciphertext, state);
-    printf("Ciphertext: %016I64x %016I64x\n", ciphertext[0], ciphertext[1]);
+    Plaintext(state, 2, plaintext, ciphertext);
+    printf("\nEncrypt: %016I64x %016I64x\n", ciphertext[0], ciphertext[1]);
     Finalization(state, key);
-    printf("Tag: %016I64x %016I64x\n", state[3], state[4]);
+    printf("tag: %016I64x %016I64x\n", state[3], state[4]);
 }
 
+void decrypt(bit64 IV, bit64 state[], bit64 key[], bit64 nonce[], bit64 plaintext[], bit64 ciphertext[], bit64 associated_data_text[])
+{
+    state[0] = IV;
+    state[1] = key[0];
+    state[2] = key[1];
+    state[3] = nonce[0];
+    state[4] = nonce[1];
+    initialization(state,key);
+    Associated(state, 3, associated_data_text);
+    print_state(state);
+    Ciphertext(state, 2, plaintext, ciphertext);
+    printf("\nDecrypt: %016I64x %016I64x\n", plaintext[0], plaintext[1]);
+    Finalization(state, key);
+    printf("tag: %016I64x %016I64x\n", state[3], state[4]);
+}
 int main() {
-    bit64 nonce[2] = {0};
-    bit64 key[2] = {0};
+    bit64 nonce[2] = { 0x0000000000000001, 0x0000000000000002 };
+    bit64 key[2] = { 0 };
     bit64 IV = 0x80400c0600000000;
-    bit64 plaintext[] = {0x1234567980abcdef, 0x82187}, ciphertext[] = {0};
-    ascon(nonce, key, IV, plaintext, ciphertext, state);
-    ascon(nonce, key, IV, ciphertext, ciphertext, state);
-    return 0;
+    bit64 plaintext[] = {0x1234567890abcdef, 0x82187};
+    bit64 ciphertext[2] = { 0 };
+    bit64 associated_data_text[] = { 0x787878, 0x878787, 0x09090};
+
+    encrypt(IV, state, key, nonce, plaintext, ciphertext, associated_data_text);
+    decrypt(IV, state, key, nonce, plaintext, ciphertext, associated_data_text);
 }
